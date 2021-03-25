517273faa9f09ccd3c13a30f7d17adee46965fe6
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2021-03-25 22:13:22 +0200

    initial commit

---
+++ RomanMath/RomanMath.Impl/Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RomanMath.Impl
+{
+	public static class Service
+	{
+		/// <summary>
+		/// See TODO.txt file for task details.
+		/// Do not change contracts: input and output arguments, method name and access modifiers
+		/// </summary>
+		/// <param name="expression"></param>
+		/// <returns></returns>
+
+		public static int Evaluate(string expression)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
