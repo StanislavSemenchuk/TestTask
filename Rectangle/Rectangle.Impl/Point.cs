912ad1e369eba56cf45358da5ad804a6ad16ad0b
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2021-03-27 15:08:40 +0200

    added checking for list of points

--- Rectangle/Rectangle.Impl/Point.cs
+++ Rectangle/Rectangle.Impl/Point.cs
@@ -4,9 +4,33 @@ using System.Text;
 
 namespace Rectangle.Impl
 {
-	public sealed class Point
+	public sealed class Point : IEquatable<Point>
 	{
 		public int X { get; set; }
 		public int Y { get; set; }
-	}
+
+        public bool Equals(Point other)
+        {
+            //Check whether the compared object is null.
+            if (Object.ReferenceEquals(other, null)) return false;
+
+            //Check whether the compared object references the same data.
+            if (Object.ReferenceEquals(this, other)) return true;
+
+            //Check whether the points' properties are equal.
+            return Y.Equals(other.Y) && X.Equals(other.X);
+        }
+
+        public override int GetHashCode()
+        {
+            //Get hash code for the X field.
+            int hashPointX = X.GetHashCode();
+
+            //Get hash code for the Y field.
+            int hashPointY = Y.GetHashCode();
+
+            //Calculate the hash code for the product.
+            return hashPointX ^ hashPointY;
+        }
+    }
 }
--- Rectangle/Rectangle.Impl/Service.cs
+++ Rectangle/Rectangle.Impl/Service.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Rectangle.Impl
 {
@@ -13,6 +14,16 @@ namespace Rectangle.Impl
 		/// <returns></returns>
 		public static Rectangle FindRectangle(List<Point> points)
 		{
+			if (points.Count < 2 || points == null || points.Distinct().Count() != points.Count()) 
+			{
+				throw new ArgumentException("Given points are invalid");
+			}
+
+			int minY = points.Select(x => x.Y).Min();
+			int maxY = points.Select(x => x.Y).Max();
+			int minX = points.Select(x => x.X).Min();
+			int maxX = points.Select(x => x.X).Max();
+
 			throw new NotImplementedException();
 		}
 	}
